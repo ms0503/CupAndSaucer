76fb580bdf611f856925474a0135983d5f253ff3
Sora Tonami <ms0503@outlook.com>
2024-06-03 16:02:33 +0900

    アセンブリ定義を追加。

--- build_package.sh
+++ build_package.sh
@@ -6,6 +6,7 @@ OUT_DIR=$BASE_DIR/out
 VERSION=$(jq -r .version $PKG_DIR/package.json)
 OUT_FILE=$OUT_DIR/ms.sora.cup-and-saucer-$VERSION.zip
 mkdir -p $OUT_DIR
+cd $BASE_DIR
 cp LICENSE.md README.md $PKG_DIR
 cd $PKG_DIR
 rm -f $OUT_FILE
