8b0911a5a527d102a3e00b42e2e8b2a2a6633681
Sora Tonami <ms0503@outlook.com>
2024-06-10 03:37:13 +0900

    Git LFSなんもわからん

--- Assets/CupAndSaucer/Runtime/Scripts/Saucer.cs
+++ Assets/CupAndSaucer/Runtime/Scripts/Saucer.cs
@@ -1,3 +1,17 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:a525b0c0984a1f6f0686f72fc8e0ff1584db36f2f5136d1365a4b89b4f465280
-size 461
+﻿using UdonSharp;
+using UnityEngine;
+
+public class Saucer : UdonSharpBehaviour {
+    [SerializeField]
+    private GameObject refillTrigger;
+
+    private bool _canRefill;
+
+    public override void Interact() {
+        var refillTriggerComponent = this.refillTrigger.GetComponent<SaucerRefillTrigger>();
+        if(!refillTriggerComponent.CanRefill) {
+            return;
+        }
+        refillTriggerComponent.cup.GetComponent<Cup>().UpdateState(0);
+    }
+}
--- build_package.sh
+++ build_package.sh
@@ -1,3 +1,13 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:ac2ecbe2858f618aa51987bdc1d95b897c8afe1d667505c335ab197557baaf7c
-size 343
+#!/usr/bin/env bash
+set -eu
+BASE_DIR=$(realpath "$(dirname "$0")")
+PKG_DIR=$BASE_DIR/Assets/CupAndSaucer
+OUT_DIR=$BASE_DIR/out
+VERSION=$(jq -r .version $PKG_DIR/package.json)
+OUT_FILE=$OUT_DIR/ms.sora.cup-and-saucer-$VERSION.zip
+mkdir -p $OUT_DIR
+cd $BASE_DIR
+cp LICENSE.md README.md $PKG_DIR
+cd $PKG_DIR
+rm -f $OUT_FILE
+7z a -r $OUT_FILE ./*
